d993cd49cb8e01ed4491bb492285e4e03af75fe0
SlyZorua <44384500+SlyZorua@users.noreply.github.com>
2023-07-02 20:45:20 +1000

    Added settings

---
+++ LiveSplit.IRLTimesave/UI/Components/IRLTimesaveSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplit.UI.Components
+{
+    public partial class IRLTimesaveSettings : UserControl
+    {
+        public bool UseStartTimerValue { get; set; }
+        public LayoutMode Mode { get; set; }
+        public IRLTimesaveSettings()
+        {
+            InitializeComponent();
+            UseStartTimerValue = true;
+        }
+
+        private void IRLTimesaveSettings_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
